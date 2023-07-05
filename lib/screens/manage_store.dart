d2d8b845edd66c5270bb62db918ad41a20fb3173
Joyal Joseph <joyaljosephnote@gmail.com>
2023-07-06 01:51:21 +0530

    added the first card

--- lib/screens/manage_store.dart
+++ lib/screens/manage_store.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ManageStore extends StatelessWidget {
@@ -6,6 +7,7 @@ class ManageStore extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 233, 232, 232),
       appBar: AppBar(
         backgroundColor: Colors.blue[800],
         title: Title(
@@ -13,6 +15,56 @@ class ManageStore extends StatelessWidget {
           child: const Text('Manage Store'),
         ),
         centerTitle: true,
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        childAspectRatio: 1.4,
+        padding: const EdgeInsets.all(10),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        children: [
+          Card(
+            semanticContainer: true,
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(15, 10, 0, 8),
+                  child: Icon(
+                    CupertinoIcons.speaker_2_fill,
+                    size: 50,
+                    color: Colors.orange,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                  child: Text(
+                    'Marketing',
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(15, 0, 0, 10),
+                  child: Text(
+                    'Designs',
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
