7eca513e86755b212400a7b556c4071cd53d9d9e
Joyal Joseph <joyaljosephnote@gmail.com>
2023-07-07 01:01:13 +0530

    bottom button and manage store pages completed

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:ui_design/screens/additional_information.dart';
 import 'package:ui_design/screens/dukaan_premium.dart';
 import 'package:ui_design/screens/manage_store.dart';
-import 'package:flutter_boxicons/flutter_boxicons.dart';
+
 import 'package:ui_design/screens/order_page.dart';
 import 'package:ui_design/screens/payments.dart';
 
@@ -36,7 +36,7 @@ class BottomTabBar extends StatefulWidget {
 class _BottomTabBarState extends State<BottomTabBar> {
   int _index = 0;
 
-  final List<Widget> _widgetOptions = <Widget>[
+  final _pages = [
     const Payments(),
     const OrderPage(),
     const DukanPremium(),
@@ -44,37 +44,38 @@ class _BottomTabBarState extends State<BottomTabBar> {
     const AdditionalInformation(),
   ];
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _index = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // ignore: constant_identifier_names
     const IconData grid_view_outlined =
         IconData(0xf0d7, fontFamily: 'MaterialIcons');
     return Scaffold(
-      body: _widgetOptions.elementAt(_index),
+      body: _pages[_index],
       bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.white,
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(Boxicons.bx_home_circle), label: 'Home'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.card_travel), label: 'Order'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.grid_view_outlined), label: 'Products'),
-          BottomNavigationBarItem(
-              icon: Icon(Boxicons.bx_list_check), label: 'Manage'),
-          BottomNavigationBarItem(
-              icon: Icon(Boxicons.bx_user), label: 'Account'),
-        ],
-        currentIndex: _index,
+        type: BottomNavigationBarType.fixed,
+        selectedFontSize: 14.0,
+        unselectedFontSize: 14.0,
+        showUnselectedLabels: true,
         selectedItemColor: Colors.blue,
         unselectedItemColor: Colors.grey,
-        onTap: _onItemTapped,
+        currentIndex: _index,
+        onTap: (value) {
+          setState(() {
+            _index = value;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_bag_outlined), label: "Order"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.grid_view_outlined), label: "Products"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.layers_rounded), label: "Manage"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_2_outlined), label: "Account"),
+        ],
       ),
     );
   }
