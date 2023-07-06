7ddf7febbf469dcf6acd8ec5a65fafb40f5f3808
Joyal Joseph <joyaljosephnote@gmail.com>
2023-07-06 15:40:00 +0530

    added the navigation also

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ui_design/screens/manage_store.dart';
+import 'package:flutter_boxicons/flutter_boxicons.dart';
+import 'package:ui_design/screens/payments.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +16,44 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primaryColor: Colors.blue[800]),
       title: 'Dukaa App UI Design',
       debugShowCheckedModeBanner: false,
-      home: const ManageStore(),
+      home: const BottomTabBar(),
+    );
+  }
+}
+
+class BottomTabBar extends StatefulWidget {
+  const BottomTabBar({super.key});
+
+  @override
+  State<BottomTabBar> createState() => _BottomTabBarState();
+}
+
+class _BottomTabBarState extends State<BottomTabBar> {
+  int _index = 0;
+  final screens = [
+    const ManageStore(),
+    const Payments(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[_index],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _index,
+        onTap: (value) {
+          setState(() {
+            _index = value;
+          });
+        },
+        backgroundColor: Colors.white,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Boxicons.bx_home_circle), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Boxicons.bx_home_circle), label: 'Home'),
+        ],
+      ),
     );
   }
 }
--- lib/screens/manage_store.dart
+++ lib/screens/manage_store.dart
@@ -19,7 +19,7 @@ class ManageStore extends StatelessWidget {
       body: GridView.count(
         crossAxisCount: 2,
         childAspectRatio: 1.4,
-        padding: const EdgeInsets.all(10),
+        padding: const EdgeInsets.all(12),
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
         children: [
@@ -29,32 +29,71 @@ class ManageStore extends StatelessWidget {
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(8.0),
             ),
-            child: const Column(
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.fromLTRB(15, 10, 0, 8),
-                  child: Icon(
-                    CupertinoIcons.speaker_2_fill,
-                    size: 50,
-                    color: Colors.orange,
+                  padding: const EdgeInsets.fromLTRB(15, 15, 0, 8),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    width: 40,
+                    height: 40,
+                    alignment: Alignment.center,
+                    child: const Icon(
+                      CupertinoIcons.speaker_2_fill,
+                      size: 25,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-                Padding(
+                const Padding(
                   padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
                   child: Text(
-                    'Marketing',
+                    'Marketing \nDesigns ',
                     style: TextStyle(
                         fontSize: 18,
                         color: Colors.black,
                         fontWeight: FontWeight.w500),
                   ),
                 ),
+              ],
+            ),
+          ),
+          Card(
+            semanticContainer: true,
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.fromLTRB(15, 0, 0, 10),
+                  padding: const EdgeInsets.fromLTRB(15, 15, 0, 8),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 38, 252, 23),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    width: 40,
+                    height: 40,
+                    alignment: Alignment.center,
+                    child: const Icon(
+                      Icons.currency_rupee_rounded,
+                      size: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
                   child: Text(
-                    'Designs',
+                    'Online \nPayments',
                     style: TextStyle(
                         fontSize: 18,
                         color: Colors.black,
